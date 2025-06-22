456c9640580276c6edf097daa2d797e26c3e84d6
Aca M <acamaulana123@gmail.com>
2025-06-22 17:30:15 +0700

    Preview full datasets

--- app/src/main/java/me/acml/predictsleepdisorder/ui/screens/datasets/DatasetsScreen.kt
+++ app/src/main/java/me/acml/predictsleepdisorder/ui/screens/datasets/DatasetsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,11 +23,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -95,21 +103,50 @@ fun DatasetsScreen(
                 datasets = datasets.slice(0..5)
             )
             Spacer(Modifier.height(5.dp))
-            Button(
-                onClick = {},
-                modifier = Modifier.fillMaxWidth(),
-                shape = PredictSleepDisorderTheme.shapes.textField
-            ) {
-                Text(
-                    text = stringResource(R.string.view_all),
-                    style = PredictSleepDisorderTheme.typography.bodyMedium.copy(
-                        fontWeight = FontWeight.SemiBold
-                    ),
-                    textAlign = TextAlign.Center
+            FullDatasets(
+                datasets = datasets
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FullDatasets(datasets: List<List<String>>) {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    Button(
+        onClick = {
+            showBottomSheet = true
+        },
+        modifier = Modifier.fillMaxWidth(),
+        shape = PredictSleepDisorderTheme.shapes.textField
+    ) {
+        Text(
+            text = stringResource(R.string.view_all),
+            style = PredictSleepDisorderTheme.typography.bodyMedium.copy(
+                fontWeight = FontWeight.SemiBold
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState
+        ) {
+            Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+                CsvTable(
+                    datasets = datasets
                 )
             }
         }
     }
+
 }
 
 @Composable
